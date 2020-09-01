b90f19bfebaa2e93896e5e3ead02608a5e234699
Truc Krystosek <destructible88@gmail.com>
2020-09-01 11:08:51 -0500

    uploading

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index', as: 'home'
+
+  get 'about' => 'pages#about', as: 'about'
+
+  resources :posts do
+  	#so it goes to /post/:id/comment/:comment_id
+  	resources :comments
+  end
+end
